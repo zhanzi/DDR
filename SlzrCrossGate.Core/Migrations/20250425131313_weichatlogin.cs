38eaf759af1fb58747139f148f2bafe1d0809827
zhanzi124 <zhanzi124@hotmail.com>
2025-04-25 13:15:32 +0000

    更新数据库结构（增加双因素验证，微信扫码登录）
    git-svn-id: https://monitor.pointlife365.net:8443/svn/%E9%A1%B9%E7%9B%AESVN/%E6%A0%87%E5%87%86(%E9%80%9A%E7%94%A8)%E7%A8%8B%E5%BA%8F/%E8%BD%AF%E4%BB%B6/SlzrCrossGate/trunk@5562 5ca9649f-f474-9a41-bf2b-fa04666fa0ae

---
+++ SlzrCrossGate.Core/Migrations/20250425131313_weichatlogin.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SlzrCrossGate.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class weichatlogin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "WechatBindTime",
+                table: "AspNetUsers",
+                type: "datetime(6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WechatNickname",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "WechatOpenId",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "WechatUnionId",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WechatBindTime",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "WechatNickname",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "WechatOpenId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "WechatUnionId",
+                table: "AspNetUsers");
+        }
+    }
+}
